8958874f5ad516bed6354c50648a7a70595730c9
Manav Vagdoda <manav@laptop.local>
2021-03-21 21:05:39 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import LoginForm from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
 
 import './App.css';
 import { Component } from 'react';
 
 const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+
     return (
         <ChatEngine
             height = '100vh'
             projectID = '74c46303-e86d-4ed3-9018-e0b1194d7d43'
-            userName = 'manav'
-            userSecret = '12345'
+            userName = {localStorage.getItem('username')}
+            userSecret = {localStorage.getItem('password')}
             renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     );
